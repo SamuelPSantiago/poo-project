4ffeb1716b8980b5e20e718fc155c8817d058d79
Samuel Pinheiro <samuelps147@gmail.com>
2026-01-10 03:05:17 -0300

    feat: add Truck class extending Vehicle

---
+++ src/model/Caminhao.java
@@ -0,0 +1,20 @@
+package model;
+
+public class Caminhao extends Veiculo {
+
+    private static final double TARIFA_HORA = 10.00;
+
+    public Caminhao(String placa, String modelo) {
+        super(placa, modelo);
+    }
+
+    @Override
+    public double calcularTarifa(long horas) {
+        return horas * TARIFA_HORA;
+    }
+
+    @Override
+    public String getTipo() {
+        return "Caminhao";
+    }
+}
